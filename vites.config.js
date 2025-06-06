f5e8ad8627534473cc450feb7e32dc17085d5428
build-africa-eng <afrcanfuture@gmail.com>
2025-06-06 08:28:42 +0300

    Update vites.config.js

--- vites.config.js
+++ vites.config.js
@@ -10,15 +10,4 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
-  optimizeDeps: {
-    include: ['jsmediatags'], // Pre-bundle jsmediatags
-    esbuildOptions: {
-      // Force CommonJS resolution for jsmediatags
-      mainFields: ['module', 'main'],
-      conditions: ['browser'],
-    },
-  },
-  ssr: {
-    noExternal: ['jsmediatags'], // Ensure jsmediatags is bundled as CommonJS
-  },
 });
